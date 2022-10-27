4f80894cd20f42a48e318ee65d2de90555794479
zradev <zlatkoradevv@gmail.com>
2022-10-27 15:19:53 +0300

    Add Car Model

---
+++ server/models/car.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+
+const carSchema = new mongoose.Schema({
+  brand: { type: String, required: true },
+  model: { type: String, required: true },
+  year: { type: Number, required: true },
+  type: { type: String, required: true },
+  price: { type: Number, required: true },
+  fuel: { type: String, required: true },
+  seats: { type: Number, required: true },
+  images: { type: [String], required: true },
+  location: { type: String, required: true },
+  description: { type: String, required: true },
+  count: { type: Number, default: 1 },
+});
+
+module.exports = mongoose.model("car", carSchema);
