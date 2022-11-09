5e44b9b350b619bda4584bcfca94032bc9eacda3
zradev <zlatkoradevv@gmail.com>
2022-11-09 22:53:35 +0200

    Add Delete functionality in Car Details Page

--- client/src/pages/details/index.tsx
+++ client/src/pages/details/index.tsx
@@ -15,6 +15,7 @@ const Index = () => {
   const [user, setUser] = useState<IUser | null>();
   const [error, setError] = useState<String>();
   const [days, setDays] = useState(1);
+  const [modal, setModal] = useState(false);
   const { id } = useParams();
   const { getUserData } = useContext(AuthContext);
   const navigate = useNavigate();
@@ -64,17 +65,71 @@ const Index = () => {
     }
   };
 
+  const handleDelete = async () => {
+    try {
+      if (user?.role === "admin") {
+        await axios.delete(
+          `${process.env.REACT_APP_SERVER_URL}/cars/delete/${car?._id}`
+        );
+        navigate("/cars");
+      }
+    } catch (error) {
+      if (user === null) {
+        setError("Error: You need to Log in to Rent a Car.");
+      } else {
+        setError(handleAxiosErrors(error));
+      }
+      window.scrollTo(0, 0);
+    }
+  };
+
   return (
     <MainLayout>
+      {modal && (
+        <div
+          className={`${
+            modal
+              ? "fixed top-0 left-0 z-[9999] w-[100%] h-[100vh] overflow-hidden bg-black bg-opacity-50 "
+              : "hidden"
+          }`}
+          onClick={() => setModal(() => false)}
+        >
+          <div className="flex flex-col p-5 absolute top-[50%] left-[50%] -translate-x-[50%] -translate-y-[50%]  w-[100%] md:w-auto h-fit md:h-auto bg-white font-bold text-lg">
+            <h1 className="mb-10">Please confirm</h1>
+            <p>Are you sure you want to delete this car?</p>
+            <div className="flex">
+              <button
+                onClick={() => setModal(false)}
+                className="flex m-auto text-black w-fit my-6 border-2 border-indigo-800 p-1 px-4 rounded-full hover:bg-sky-100"
+              >
+                Cancel
+              </button>
+              <button
+                onClick={handleDelete}
+                className="flex m-auto text-white bg-sky-800 w-fit my-6 border-2 border-indigo-800 p-1 px-4 rounded-full hover:bg-sky-700"
+              >
+                Confirm
+              </button>
+            </div>
+          </div>
+        </div>
+      )}
       {car ? (
         <div className="flex flex-col gap-5 md:pЬ-[80px] mt-3">
-          {user && (
-            <Link
-              to={`/cars/edit/${car?._id}`}
-              className="w-fit self-center mt-5 border border-gray-400 hover:border-gray-500 px-6 py-1 bg-gray-100 hover:bg-gray-200"
-            >
-              EDIT
-            </Link>
+          {user?.role === "admin" && (
+            <div className="self-center">
+              <Link to={`/cars/edit/${car?._id}`}>
+                <button className="w-fit self-center mt-5 border rounded-full border-gray-400 hover:border-gray-500 px-6 py-1 bg-gray-100 hover:bg-gray-200 m-5">
+                  EDIT
+                </button>
+              </Link>
+              <button
+                className="w-fit self-center mt-5 border rounded-full border-gray-400 hover:border-gray-500 px-6 py-1 bg-gray-100 hover:bg-gray-200 m-5"
+                onClick={() => setModal(true)}
+              >
+                DELETE
+              </button>
+            </div>
           )}
           {error && (
             <div className="text-2xl text-center text-rose-400">{error}</div>
