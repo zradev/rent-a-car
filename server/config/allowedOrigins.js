ae19fd6f01466037e0243e1ff07ba178f6a03d3a
zradev <zlatkoradevv@gmail.com>
2022-11-14 00:14:02 +0200

    Add Origin

--- server/config/allowedOrigins.js
+++ server/config/allowedOrigins.js
@@ -3,6 +3,7 @@ const allowedOrigins = [
   "http://127.0.0.1:5500",
   "http://localhost:3500",
   "http://localhost:3000",
+  "https://rent-a-car-b4150.web.app",
 ];
 
 module.exports = allowedOrigins;
